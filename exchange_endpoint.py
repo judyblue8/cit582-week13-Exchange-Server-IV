aef4a6a609d4419511358be8aac59cb3c982c24c
judyblue8 <74081546+judyblue8@users.noreply.github.com>
2022-04-10 12:53:41 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -32,7 +32,13 @@ def shutdown_session(response_or_exc):
 """ Suggested helper methods """
 
 def check_sig(payload,sig):
-    pass
+
+    if payload['platform'] == 'Ethereum':
+        eth_msg = eth_account.messages.encode_defunct(text=payload)
+        return eth_account.Account.recover_message(eth_msg, signature=sig) == payload['sender_pk']
+    else:
+        return  algosdk.util.verify_bytes(payload.encode('utf-8'), sig, payload['sender_pk'])
+
 
 def fill_order(order,txes=[]):
     pass
@@ -40,10 +46,51 @@ def fill_order(order,txes=[]):
 def log_message(d):
     # Takes input dictionary d and writes it to the Log table
     # Hint: use json.dumps or str() to get it in a nice string form
-    pass
+    g.session.add(Log(logtime=datetime.now(), message=json.dumps(d)))
+    g.session.commit()
 
 """ End of helper methods """
+def process_order(order):
 
+    session=g.session
+    new_order = order
+    session.add(new_order)
+    session.commit()
+    # check if there are any existing order that matches
+    while (new_order is not None):
+        for existing_order in session.query(Order).filter(Order.filled == None).all():
+            # if a good rate found,set to filled
+            if existing_order.filled == None and existing_order.buy_currency == new_order.sell_currency and \
+                    existing_order.sell_currency == new_order.buy_currency and \
+                    existing_order.sell_amount / existing_order.buy_amount >= new_order.buy_amount / new_order.sell_amount:
+                new_order.filled = datetime.now()
+                existing_order.filled = datetime.now()
+                existing_order.counterparty_id = new_order.id
+                new_order.counterparty_id = existing_order.id
+
+                if new_order.buy_amount < existing_order.sell_amount:
+                    remaining_sell = existing_order.sell_amount - new_order.buy_amount
+                    remaining_buy =  remaining_sell/(existing_order.sell_amount / existing_order.buy_amount)
+                    new_order = Order(sender_pk=existing_order.sender_pk, receiver_pk=existing_order.receiver_pk,
+                                      buy_currency=existing_order.buy_currency,
+                                      sell_currency=existing_order.sell_currency, buy_amount=remaining_buy,
+                                      sell_amount=remaining_sell, creator_id=existing_order.id, filled=None)
+                    session.add(new_order)
+                    session.commit()
+                elif new_order.buy_amount>existing_order.sell_amount :
+                    remaining_buy = new_order.buy_amount - existing_order.sell_amount
+                    remaining_sell = remaining_buy/(new_order.buy_amount/ new_order.sell_amount)
+                    new_order = Order(sender_pk=new_order.sender_pk,receiver_pk=new_order.receiver_pk,
+                                        buy_currency=new_order.buy_currency, sell_currency=new_order.sell_currency,
+                                        sell_amount= remaining_sell, buy_amount=remaining_buy, creator_id=new_order.id, filled=None)
+                    session.add(new_order)
+                    session.commit()
+
+                else:
+                    # exact amount,no child
+                    new_order = None
+                break
+        return
 
 
 @app.route('/trade', methods=['POST'])
@@ -72,19 +119,52 @@ def trade():
         #Your code here
         #Note that you can access the database session using g.session
 
+
+        signature = content['sig']
+        payload = json.dumps(content['payload'])
+        payload_1=content['payload']
+        sender_pk = content['payload']['sender_pk']
+        receiver_pk = content['payload']['receiver_pk']
+        buy_currency = content['payload']['buy_currency']
+        sell_currency = content['payload']['sell_currency']
+        buy_amount = content['payload']['buy_amount']
+        sell_amount = content['payload']['sell_amount']
+        platform = content['payload']['platform']
         # TODO: Check the signature
-        
-        # TODO: Add the order to the database
-        
-        # TODO: Fill the order
-        
-        # TODO: Be sure to return jsonify(True) or jsonify(False) depending on if the method was successful
+
+
+        if check_sig(payload,signature):
+            #print(check_sig(payload,sender_pk))
+            # TODO: Fill the order
+            process_order(Order(sender_pk=sender_pk, receiver_pk=receiver_pk, buy_currency=buy_currency,sell_currency=sell_currency, buy_amount=buy_amount, sell_amount=sell_amount,
+                                signature=signature))
+            return jsonify(True)
+        else:
+            log_message(content)
+            return jsonify(False)
+
+
         
 
 @app.route('/order_book')
 def order_book():
     #Your code here
     #Note that you can access the database session using g.session
+    orders = g.session.query(Order).filter().all()
+    lst = []
+
+    for o in orders:
+        order = {}
+        order['sender_pk'] = o.sender_pk
+        order['receiver_pk'] = o.receiver_pk
+        order['buy_currency'] = o.buy_currency
+        order['sell_currency'] = o.sell_currency
+        order['buy_amount'] = o.buy_amount
+        order['sell_amount'] = o.sell_amount
+        order['signature'] = o.signature
+        lst.append(order)
+    result = {}
+    result['data'] = lst
     return jsonify(result)
 
 if __name__ == '__main__':
